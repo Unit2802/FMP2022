ce058165805e35c3bbb6d5d570db7163fbfa6e88
Urosh <urosh.gazo@gmail.com>
2022-03-11 10:33:17 +0000

    Stuff

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,22 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
-using Photon.Realtime;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
-    public static Launcher Instance;
-
     [SerializeField] TMP_InputField roomNameInputField;
-    [SerializeField] TMP_Text errorText;
-    [SerializeField] TMP_Text roomNameText;
-    [SerializeField] Transform roomListContent;
-    [SerializeField] GameObject roomListItemPrefrab;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
     void Start()
     {
         Debug.Log("Connecting to Master");
@@ -41,55 +29,6 @@ public class Launcher : MonoBehaviourPunCallbacks
     }
     public void CreateRoom()
     {
-        if(string.IsNullOrEmpty(roomNameInputField.text))
-        {
-            return;
-        }
-        PhotonNetwork.CreateRoom(roomNameInputField.text);
-        MenuManager.Instance.OpenMenu("Loading");
-
         
-    }
-
-    public override void OnJoinedRoom()
-    {
-        MenuManager.Instance.OpenMenu("Room");
-        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
-    }
-
-    public override void OnCreateRoomFailed(short returnCode, string message)
-    {
-        errorText.text = "Failed to create room: " + message;
-        MenuManager.Instance.OpenMenu("Error");
-
-    }
-
-    public void LeaveRoom()
-    {
-        PhotonNetwork.LeaveRoom();
-        MenuManager.Instance.OpenMenu("Loading");
-    }
-
-    public void JoinRoom(RoomInfo info)
-    {
-        PhotonNetwork.JoinRoom(info.Name);
-        MenuManager.Instance.OpenMenu("Loading");
-    }
-
-    public override void OnLeftRoom()
-    {
-        MenuManager.Instance.OpenMenu("Title");
-    }
-
-    public override void OnRoomListUpdate(List<RoomInfo> roomList)
-    {
-        foreach(Transform trans in roomListContent)
-        {
-            Destroy(trans.gameObject);
-        }
-        for (int i = 0; i < roomList.Count; i++)
-        {
-            Instantiate(roomListItemPrefrab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
-        }
     }
 }
