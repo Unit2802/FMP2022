138d333f01a71b8f4d20d86df54fe2b116ac59d6
Urosh <urosh.gazo@gmail.com>
2022-03-18 09:05:40 +0000

    Added a movement script I want to try add in with the multiplayer

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -68,6 +68,11 @@ public class Launcher : MonoBehaviourPunCallbacks
 
         Player[] players = PhotonNetwork.PlayerList;
 
+        foreach(Transform child in PlayerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
         for (int i = 0; i < players.Count(); i++)
         {
             Instantiate(playerListItemPrefrab, PlayerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
@@ -121,6 +126,9 @@ public class Launcher : MonoBehaviourPunCallbacks
         }
         for (int i = 0; i < roomList.Count; i++)
         {
+
+            if (roomList[i].RemovedFromList)
+                continue;
             Instantiate(roomListItemPrefrab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
         }
     }
