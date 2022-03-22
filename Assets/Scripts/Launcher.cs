5b3148c6ef15db0eec1816af128b9157d10f5258
Urosh <urosh.gazo@gmail.com>
2022-03-22 09:47:40 +0000

    Revert "Merge branch 'main' of https://github.com/Unit2802/FMP2022"
    This reverts commit 3100691b75c5836ca75ae36fdbd691640d03cd76, reversing
    changes made to 511870cd6972a1d9ce71d0943cdb05cc1ea967b7.

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
+using System.Linq;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+
     [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] Transform PlayerListContent;
+    [SerializeField] GameObject roomListItemPrefrab;
+    [SerializeField] GameObject playerListItemPrefrab;
+    [SerializeField] GameObject startGameButton;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     void Start()
     {
         Debug.Log("Connecting to Master");
@@ -14,21 +30,111 @@ public class Launcher : MonoBehaviourPunCallbacks
         PhotonNetwork.ConnectUsingSettings();
     }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected to Master");
 
         PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     public override void OnJoinedLobby()
     {
         MenuManager.Instance.OpenMenu("Title");
         Debug.Log("Joined Lobby");
+        PhotonNetwork.NickName = "Player " + Random.Range(0, 1000).ToString("0000");
     }
     public void CreateRoom()
     {
+        if(string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("Loading");
+
         
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("Room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        foreach(Transform child in PlayerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Instantiate(playerListItemPrefrab, PlayerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+
+        }
+
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Failed to create room: " + message;
+        MenuManager.Instance.OpenMenu("Error");
+
+    }
+
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("Loading");
+
+        
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("Title");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+        for (int i = 0; i < roomList.Count; i++)
+        {
+
+            if (roomList[i].RemovedFromList)
+                continue;
+            Instantiate(roomListItemPrefrab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(playerListItemPrefrab, PlayerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
     }
 }
