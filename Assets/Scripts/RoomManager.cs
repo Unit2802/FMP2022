5f9b3dc60ab0ef01ecfeeb4ad57ef3cc59ae04a4
Urosh <urosh.gazo@gmail.com>
2022-03-24 10:25:54 +0000

    Revert "Merge branch 'main' of https://github.com/Unit2802/FMP2022"
    This reverts commit b89c73f783929b3eccca61685c24698f1ce0a26c, reversing
    changes made to 9e40c3d5c6924e119df5b4d8afe257f2553e4304.

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -7,10 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
-    public float xSense = 30f;
-    public float ySense = 30f;
-
-
 
     public static RoomManager Instance;
 
--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,28 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
-
-
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
-    }
-    
-    public void SetXSense (float x)
-    {
-       
-        RoomManager.Instance.xSense = x;
-
-    }
-    public void SetYSense (float y)
-    {
-        Debug.Log(y);
-        RoomManager.Instance.ySense = y;
     }
 }
