06bbce80cf250d5369c22a203bad96aa75cdbc14
Urosh <urosh.gazo@gmail.com>
2022-03-25 09:29:25 +0000

    Added ADS and changed the menu
    Added a main menu along side the multiplayer menu, Also added aiming down sights

---
+++ Assets/Scripts/ADS.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimDownSights : MonoBehaviour
+{
+
+    private Vector3 originalPos;
+    [SerializeField] Vector3 aimPos;
+    [SerializeField] float adsSpeed = 8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ADS();
+    }
+
+    private void ADS()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, aimPos, Time.deltaTime * adsSpeed);
+        }
+        else
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, originalPos, Time.deltaTime * adsSpeed);
+        }
+    }
+}
