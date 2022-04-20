57a0d29c74d0129c2edc51d35bbe3b73eb449d94
Denis <92369626+Denis999L@users.noreply.github.com>
2022-04-20 10:07:07 +0100

    jump pad

---
+++ Assets/Scripts/JumpBoost.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBoost : MonoBehaviour
+{
+   [Range(100, 1000)]
+   public float bounceheight;
+
+
+   void OnCollisionEnter(Collision collision)
+   {
+		GameObject bouncer = collision.gameObject;
+		Rigidbody rb = bouncer.GetComponent<Rigidbody>();
+		rb.AddForce(Vector3.up * bounceheight);
+   }
+}
