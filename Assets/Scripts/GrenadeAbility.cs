dfb948550909fb96a4b5800614ba3fb46f328dc3
Urosh <urosh.gazo@gmail.com>
2022-04-20 10:36:38 +0100

    In Progress Grenade

--- Assets/Scripts/GrenadeAbility.cs
+++ Assets/Scripts/GrenadeAbility.cs
@@ -5,12 +5,44 @@ using UnityEngine;
 [CreateAssetMenu]
 public class GrenadeAbility : Ability
 {
-  
+    public float delay = 3f;
 
+    public GameObject grenade;
+
+    public GameObject explosionEffect;
+
+    float countdown;
+    bool hasExploded = false;
+
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, grenade.transform.position, grenade.transform.rotation );
+
+        //Get nearby objects
+        //Add force
+        //Damage effect
+
+        Destroy(grenade);
+    }
 
     public override void Activate(GameObject parent)
     {
-
+        if(countdown <= 0 && !hasExploded)
+        {
+            hasExploded = true;
+            Explode();
+        }
     }
 
 
