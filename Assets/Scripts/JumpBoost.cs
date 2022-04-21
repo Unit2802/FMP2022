5cdba7ba861163de6abe8b3314d7e443f812a4be
Denis <92369626+Denis999L@users.noreply.github.com>
2022-04-21 10:35:16 +0100

    implemented canvas in main scene

--- Assets/Scripts/JumpBoost.cs
+++ Assets/Scripts/JumpBoost.cs
@@ -6,13 +6,12 @@ public class JumpBoost : MonoBehaviour
 {
    [Range(100, 10000)]
    public float bounceheight;
-   PlayerController player;
 
 
 
    private void OnCollisionEnter(Collision collision)
    {
-		player.SetGroundedState(true);
+		
 		GameObject bouncer = collision.gameObject;
 		Rigidbody rb = bouncer.GetComponent<Rigidbody>();
 		rb.AddForce(Vector3.up * bounceheight);
