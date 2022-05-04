fe92d04d0a576e0b5d43a8c9eac81607d68c1b13
Denis <92369626+Denis999L@users.noreply.github.com>
2022-05-04 10:38:39 +0100

    elevator extra

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Elevator : MonoBehaviour
 {
-    public GameObject moveplatform;
+    public int maxHeight;
+    public int minHeight;
+    public int timeToMove;
    
 
 
-    private void OnTriggerStay()
+    private void OnTriggerStay(Collider other)
     {
-        moveplatform.transform.position += moveplatform.transform.up * Time.deltaTime;
+       if (other.tag == "Player")
+       {
+              if(Input.GetKey(KeyCode.UpArrow))
+              {
+                    if(gameObject.transform.position.y < maxHeight)
+                    {
+                        transform.DOMoveY(maxHeight, timeToMove);
+                    }
+              }
+       }      if(Input.GetKey(KeyCode.DownArrow))
+              {
+                  if(gameObject.transform.position.y > minHeight)
+                  {
+                    transform.DOMoveY(minHeight, timeToMove * 2);
+                  }
+                 
+              }
     }
 
-    private void OnTriggerStay()
-    {
-        moveplatform.transform.position += moveplatform.transform.down * Time.deltaTime;
-    }
+    
 }
