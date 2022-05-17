ab346c3100416a77725ccf37afccbbc38c52de93
Urosh <urosh.gazo@gmail.com>
2022-05-17 09:25:56 +0100

    Revert "d"
    This reverts commit a84d195c04f6c3a04dcdc49c187f38b94175cf86.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,55 +7,21 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider; 
     public Gradient gradient;
-   
-    public Image healthBar;
-    public float healthAmount = 100;
+    public Image fill;
 
-
-    private void Update()
-    {
-        if(healthAmount <= 0)
-        {
-            Application.LoadLevel(Application.loadedLevel);
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            TakeDamage(20);
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Healing(10);
-        }
-    }
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
 
-        healthBar.color = gradient.Evaluate(1f);
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
 
-        healthBar.color = gradient.Evaluate(slider.normalizedValue);
-    }
-
-    public void TakeDamage(float Damage)
-    {
-        healthAmount -= Damage;
-        healthBar.fillAmount = healthAmount / 100;
-    }
-
-    public void Healing(float healPoints)
-    {
-        healthAmount += healPoints;
-        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
-
-        healthBar.fillAmount = healthAmount / 100;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
